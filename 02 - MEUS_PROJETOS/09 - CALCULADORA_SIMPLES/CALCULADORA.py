7fefbec7d746d4116e8702af771d5f0b3ad58d0a
Ezequiel Cleydenilson Cunha Moreira <138903101+Ezequiel-Cley@users.noreply.github.com>
2024-04-04 15:40:33 -0300

    Atualizar Calculadora

--- 02 - MEUS_PROJETOS/09 - CALCULADORA_SIMPLES/CALCULADORA.py
+++ 02 - MEUS_PROJETOS/09 - CALCULADORA_SIMPLES/CALCULADORA.py
@@ -1,4 +1,3 @@
-import math
 # Programa de Calculadora Simples
 
 # Cria um Dicionario para determinar cada operação Matemática da Calculadora
@@ -24,33 +23,36 @@ while (operacao < 1) or (operacao > 4):
     operacao = int(input('Escolha a opção de 1 á 4: '))
     print('----------------------------------------')
 
+# Imprimir para o usuário a opção selecionada e qual calculo ela representa
 for i,j in oper.items():
-    #print(f'{i} e {j}')
     if i == operacao:
-        print(f'Opação informada foi {operacao} que representa a operação de {j}')
+        print(f'Opção informada foi {operacao} que representa a operação de {j}')
 
 print('----------------------------------------')
 print('Selecione com quantos números desejada Realizar a operação')
 print('----------------------------------------')
 
+# Define com quantos números deseja realizar a operação Matematica desejada
 tam = tam = int(input('Selecione a opção de 2 até 10 números: '))
 print('----------------------------------------')
+
+# Lógica para impedir que o usuário escolha um opção invalida
 while (tam < 2) or (tam > 10):
     print('Você Selecionou um número invalido deve escolher um número entre 2 e 10 !')
     print('----------------------------------------')
     tam = int(input('Selecione a opção de 2 até 10 números: '))
-        
 print('----------------------------------------')
 
+# Cria uma lista vazia para acresentar os números digitados pelo usuário
 lista_de_numeros = []
 
+# Loop para receber os números que devem ser calculados;
 for i in range(0, tam):
-    num = int(input(f'Selecione o número de posição {i+1} para realizar o calculo: '))
+    num = float(input(f'Selecione o número de posição {i+1} para realizar o calculo: '))
     print('----------------------------------------')
     lista_de_numeros.append(num)
 
-
-
+# Cria uma função para que realize o calculo de acordo com opção desejada
 def calcular(op, numeros):
     resultado = numeros[0]  # Começa com o primeiro número da lista
 
@@ -85,10 +87,11 @@ def calcular(op, numeros):
 
     return resultado
 
-teste =  calcular(operacao, lista_de_numeros)
-
+# Chamado a função passado os parametros criado anteriormente
+rst =  calcular(operacao, lista_de_numeros)
 print('----------------------------------------')
 
-print(f'O resultado do seu calculo é de {int(teste)}')
+# Define texto para apresentar o resultado para o usuário
+print(f'O resultado do seu calculo é de {round(float(rst),6)}')
 
 print('----------------------------------------')
